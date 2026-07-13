5f1bd8c6e8534c705881cd50cede58df74927751
Ayesha-Luqman <ayeshaluqman333@gmail.com>
2026-07-13 14:26:40 +0500

    Prepare Django for deployment

--- backend/ecommerce/settings.py
+++ backend/ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yiak)11$dn@xos9-rf%@v-1o2ptfj&$7*x6s4dy@ou2&ttp+!*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -136,3 +137,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:5174",
 ]
+
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
